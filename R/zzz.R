bfb996ccc7801c48a96221f83e50477b140c05f6
oblarquez <blarquez@gmail.com>
2013-10-19 12:28:54 +0200

    up191013

--- R/zzz.R
+++ R/zzz.R
@@ -3,17 +3,17 @@
  #data(paleofiresites, package=package, envir=parent.env(environment()))
 } 
 .onLoad <- function(lib, pkg) {
-   fileURLsites <- "http://blarquez.com/public/data/paleofiresites.rda"
-   #setInternet2(TRUE)
-   download.file(fileURLsites ,destfile="./data/paleofiresites.rda",method="auto")
-   fileURLdata <- "http://blarquez.com/public/data/paleofiredata.rda"
-   #setInternet2(TRUE)
-   download.file(fileURLdata ,destfile="./data/paleofiredata.rda",method="auto")
-
-   gcd <- "http://blarquez.com/public/data/GCDver.rda"
-   #setInternet2(TRUE)
-   download.file(gcd ,destfile="./data/GCDver.rda",method="auto")
-   load("./data/GCDver.rda")
-   packageStartupMessage("Downloaded and installed GCD v",GCDver$ver, ",", GCDver$date)
-   rm(GCDver)
+#    fileURLsites <- "http://blarquez.com/public/data/paleofiresites.rda"
+#    #setInternet2(TRUE)
+#    download.file(fileURLsites ,destfile="./data/paleofiresites.rda",method="auto")
+#    fileURLdata <- "http://blarquez.com/public/data/paleofiredata.rda"
+#    #setInternet2(TRUE)
+#    download.file(fileURLdata ,destfile="./data/paleofiredata.rda",method="auto")
+# 
+#    gcd <- "http://blarquez.com/public/data/GCDver.rda"
+#    #setInternet2(TRUE)
+#    download.file(gcd ,destfile="./data/GCDver.rda",method="auto")
+#    load("./data/GCDver.rda")
+#    packageStartupMessage("Downloaded and installed GCD v",GCDver$ver, ",", GCDver$date)
+#    rm(GCDver)
 } 
