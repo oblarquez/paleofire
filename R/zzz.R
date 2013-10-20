95d58441f25247d5b50d6f4e7f9fabee789d34d1
oblarquez <blarquez@gmail.com>
2013-10-20 11:37:11 +0200

    up201013

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,34 @@
   packageStartupMessage("This is paleofire v",utils::packageDescription("paleofire",field="Version"),appendLF = TRUE)
   CheckVersionFirst()
 } 
+
+CheckVersionFirst <- function() {
+  # Check to see if installed
+  if (!"GCD" %in% utils::installed.packages()[, 1]) {
+    Checks <- "Failed"
+  } else {
+    # Compare version numbers
+    temp <- getURL("https://raw.github.com/paleofire/GCD/master/DESCRIPTION")      
+    CurrentVersion <- gsub("^\\s|\\s$", "", 
+                           gsub(".*Version:(.*)\\nDate.*", "\\1", temp))
+    
+    if (utils::packageVersion("GCD") == CurrentVersion) {
+      Checks <- "Passed"
+    }
+    if (utils::packageVersion("GCD") < CurrentVersion) {
+      Checks <- "Failed"
+    }
+  }
+  
+  switch(
+    Checks,
+    Passed = { message("Everything looks OK! GCD up to date: v",CurrentVersion) },
+    Failed = {
+      ans = readline(
+        "'GCD is either outdated or not installed. Update now? (y/n) ")
+      if (ans != "y")
+        return(invisible())
+      install_github("GCD", "paleofire")
+    })
+  # Some cool things you want to do after you are sure the data is there
+}
